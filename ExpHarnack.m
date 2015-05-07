405b93bc0e977c4e4bbce18e0e63ed9837250452
mpgetz <mattpgetz@gmail.com>
2015-05-07 13:33:36 -0400

    minor edits to harnack

--- ExpHarnack.m
+++ ExpHarnack.m
@@ -1,6 +1,8 @@
 delta = input('Choose error delta, 0 < delta <= 1 ' );
 decimal_digits = input('Enter number of decimal digits of delta: ');
 trials = input('Enter number of trials to run: ');
+size = input('Enter size of grid to compute over: ');
+
 g_min = zeros(1, trials);
 g_max = zeros(1, trials);
 t_min = zeros(1, trials);
@@ -11,14 +13,14 @@ H_t = zeros(1, trials);
 for i = 1:trials
 %Generate Tiling Harmonic values on rectangular grid
 %Saves both tiling harmonic and graph harmonic inner values
-    B = rand(30);
+    B = rand(size);
     G = graphHarmInner(B, decimal_digits);
     T = findAllInnerVals(round(G, decimal_digits), delta);
-    g_min(i) = min(min(G(2:19, 2:19)));
-    g_max(i) = max(max(G(2:19, 2:19)));
+    g_min(i) = min(min(G(2:size-1, 2:size-1)));
+    g_max(i) = max(max(G(2:size-1, 2:size-1)));
     H_g(i) = g_max(i)/g_min(i);
-    t_min(i) = min(min(T(2:19, 2:19)));
-    t_max(i) = max(max(T(2:19, 2:19)));
+    t_min(i) = min(min(T(2:size-1, 2:size-1)));
+    t_max(i) = max(max(T(2:size-1, 2:size-1)));
     if t_min(i) == 0
         H_t(i) = NaN;
     else
